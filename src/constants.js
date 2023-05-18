34d79569a250158b4a08d7e1ccfcb6e5495ad335
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-05-18 16:27:32 +0200

    add start features

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const URL = 'https://api.themoviedb.org/3/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const API_KEY = 'e56bac74c49b45071b964a5f7a0906aa';
+export const LANGUAGE = 'en-US';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+//PLUGINS
+import axios from 'axios';
+
+//CSS / SCSS
 import './sass/main.scss';
+
+//IMPORTS FROM INTERNAL JS FILES
+import { URL, IMG_URL, API_KEY, LANGUAGE } from './constants';
