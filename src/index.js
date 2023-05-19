92d3aa32ef42b6ae612fa96849b141346565843a
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-05-19 20:19:18 +0200

    New project structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-//PLUGINS
-import axios from 'axios';
-
-//CSS / SCSS
-import './sass/main.scss';
-
-//IMPORTS FROM INTERNAL JS FILES
-import { URL, IMG_URL, API_KEY, LANGUAGE } from './constants';
+import './js/main'
