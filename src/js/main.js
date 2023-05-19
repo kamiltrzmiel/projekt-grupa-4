fc9c583c1326a7f1c2a2d130262d654c754c1819
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-05-19 22:35:28 +0200

    FT-04 footer
    +utworzenie footera (mobile, tablet, desktop)
    +utworzenie okna modalnego
    +wstępny zarys styli okna modalnego

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 /* -------------------------------- VARIABLES ------------------------------- */
-import './variables/constants'
+import './variables/constants';
 
 /* --------------------------------- HEADER --------------------------------- */
 
 /* --------------------------------- FOOTER --------------------------------- */
+const dialog = document.getElementById('footer-dialog');
+const hideDialogBtn = document.getElementById('hide');
+const showDialogBtn = document.getElementById('show');
+
+showDialogBtn.addEventListener('click', () => dialog.showModal());
+hideDialogBtn.addEventListener('click', () => dialog.close());
