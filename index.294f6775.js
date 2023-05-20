7b22b0dc941ca83530ffdc8f98593fe4ca2681bf
kamiltrzmiel <81704799+kamiltrzmiel@users.noreply.github.com>
2023-05-20 07:16:24 +0000

    Deploying to gh-pages from @ kamiltrzmiel/projekt-grupa-4@f878791eae092e657ff414608302d9e215d81424 🚀

---
+++ index.294f6775.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("footer-dialog"),t=document.getElementById("hide");document.getElementById("show").addEventListener("click",(()=>e.showModal())),t.addEventListener("click",(()=>e.close()));
+//# sourceMappingURL=index.294f6775.js.map
