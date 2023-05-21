7325ffe3c0e274fbaeff599fdda101ba78c07ed0
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-05-21 06:12:27 +0200

    Corrected js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
 /* -------------------------------- VARIABLES ------------------------------- */
-import './variables/constants';
+import { API_URL, IMG_URL, API_KEY, LANGUAGE } from './variables/constants';
 
 /* --------------------------------- HEADER --------------------------------- */
 
 /* --------------------------------- FOOTER --------------------------------- */
-import './footer/footer';
+const dialog = document.getElementById('footer-dialog');
+const hideDialogBtn = document.getElementById('hide');
+const showDialogBtn = document.getElementById('show');
+const body = document.querySelector("body");
+
+showDialogBtn.addEventListener('click', () => {
+  body.style.overflow = 'hidden';
+  dialog.showModal()
+});
+hideDialogBtn.addEventListener('click', () => {
+  body.style.overflow = 'auto';
+  dialog.close()
+});
+
--- src/js/variables/constants.js
+++ src/js/variables/constants.js
@@ -1,4 +1,6 @@
-export const URL = 'https://api.themoviedb.org/3/';
+export const API_URL = 'https://api.themoviedb.org/3/';
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 export const API_KEY = 'e56bac74c49b45071b964a5f7a0906aa';
 export const LANGUAGE = 'en-US';
+
+export default { API_URL, IMG_URL, API_KEY, LANGUAGE }
