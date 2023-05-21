243c3b347a07a6c3f9a5d3e2295b1d1f8b0e20f5
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-05-21 06:01:26 +0200

    Changed styles in modal

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,4 @@ import './variables/constants';
 /* --------------------------------- HEADER --------------------------------- */
 
 /* --------------------------------- FOOTER --------------------------------- */
-const dialog = document.getElementById('footer-dialog');
-const hideDialogBtn = document.getElementById('hide');
-const showDialogBtn = document.getElementById('show');
-
-showDialogBtn.addEventListener('click', () => dialog.showModal());
-hideDialogBtn.addEventListener('click', () => dialog.close());
+import './footer/footer';
