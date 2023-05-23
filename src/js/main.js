197ae29871bfccd67bb08bc5de90173cc144c6f8
Bartosz-Zarski <117305195+Bartosz-Zarski@users.noreply.github.com>
2023-05-23 22:19:49 +0200

    Added modal close js function

--- src/js/main.js
+++ src/js/main.js
@@ -24,6 +24,17 @@ const closeModal = () => {
 hideDialogBtn.addEventListener('click', closeModal);
 dialog.addEventListener('close', closeModal);
 
+/* --------------------------------- MOVIE-MODAL --------------------------------- */
+
+const hideModalBtn = document.getElementById('hide-modal');
+const movieModal = document.getElementById('modal-backdrop');
+
+const toggleModal = () => {
+  movieModal.classList.toggle('hidden');
+};
+
+hideModalBtn.addEventListener('click', toggleModal);
+
 /* ------------------------------ USAGE EXAMPLE ----------------------------- */
 import api from './services/api';
 import { render } from './services/render';
