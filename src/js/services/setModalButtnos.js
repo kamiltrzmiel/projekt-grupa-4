82fd34aa423eabe3bb80524faaf3de83f4705c63
Krzysztof R <plkrozbicki@gmail.com>
2023-05-23 14:43:50 +0200

    added Comments for team

--- src/js/services/setModalButtnos.js
+++ src/js/services/setModalButtnos.js
@@ -1,5 +1,11 @@
 import { UserMovies } from './userMovies';
+
+//tworzy nowy obiekt który ma w sobie dane i medoty z localStorage
 export const userMovies = new UserMovies();
+
+//Funkcja pobiera z dokumentu przyciski dodania do obejrzanych i do kolejki oraz przypisuje im funkcje w eventListnerach
+//Funkcja przyjmuje jako argument obiekt - w domyśle dane pobrane z TMDB dane jednego filmu
+//dlatego najlepiej dorzucić ją do funkcji tworzenia Modala która powinna operować na tym samym obiekcie.
 
 export const setModalButtons = data => {
   const watchBtn = document.getElementById('modal-watched');
