1fd1a11a7d9000f51bb56180a6045efba9fba8b7
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-05-25 22:34:05 +0200

    feature/add fetch trailer to api.js crt trailer.js

--- src/js/services/api.js
+++ src/js/services/api.js
@@ -2,28 +2,22 @@ import axios from 'axios';
 import { API_KEY } from '../variables/constants';
 
 export const fetchTrendingMovies = async pageNumber => {
-  const response = await axios.get(
-    'https://api.themoviedb.org/3/movie/popular',
-    {
-      params: {
-        api_key: API_KEY,
-        page: pageNumber,
-      },
-    }
-  );
+  const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
+    params: {
+      api_key: API_KEY,
+      page: pageNumber,
+    },
+  });
   return response;
 };
 
 export const fetchMoviesWithQuery = async searchQuery => {
-  const response = await axios.get(
-    'https://api.themoviedb.org/3/search/movie',
-    {
-      params: {
-        api_key: API_KEY,
-        query: searchQuery,
-      },
-    }
-  );
+  const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+    params: {
+      api_key: API_KEY,
+      query: searchQuery,
+    },
+  });
   return response;
 };
 
@@ -36,10 +30,20 @@ export const fetchMovieById = async id => {
   return response;
 };
 
+export const fetchTrailerById = async id => {
+  const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos`, {
+    params: {
+      api_key: API_KEY,
+    },
+  });
+  return response;
+};
+
 const api = {
   fetchTrendingMovies,
   fetchMoviesWithQuery,
   fetchMovieById,
+  fetchTrailerById,
 };
 
 export default api;
