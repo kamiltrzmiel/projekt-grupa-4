7cb53297eddf62abc94ca07c33dda907f95b9605
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-05-25 22:53:07 +0200

    fetaure/prepare schem trailer.js

--- src/js/services/trailer.js
+++ src/js/services/trailer.js
@@ -1,1 +1,15 @@
-//import api from './api';
+import api from './api';
+
+const defTrailerUrl = 'https://www.youtube.com/embed/';
+const trailerEl = document.querySelector('.tariler-btn');
+
+const fetchTrailerById = async id => {
+  try {
+    const response = await api.fetchTrailerById(id);
+    const item = response.data;
+    //...//
+  } catch (error) {
+    console.log(error);
+  }
+};
+fetchTrailerById(id);
