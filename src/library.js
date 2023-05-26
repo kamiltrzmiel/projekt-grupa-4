c1a3db23d56e3428f49674d48dc570f97890af7d
Kamila Adamska <121kama121@gmail.com>
2023-05-26 02:53:46 +0200

    Added loader to library
    Tym razem bez settimeouta, bo tu i tak biblioteka się chwilę ładuje ;) Nie wiem tylko czy loader będzie się wyświetlał przy przełączaniu do queue, bo nie znalazłam gdzie to jest podłączone.

--- src/library.js
+++ src/library.js
@@ -2,10 +2,12 @@ import './sass/main.scss';
 import './js/services/detailModal';
 import { reloadHeader, setRegisterAndSignUp } from './js/services/firebase';
 import { initializeLibrary } from './js/services/loadMovies';
+import { moviesLoading } from './js/services/loader';
 
 const containerEl = document.getElementById('posters');
 
 reloadHeader();
 setRegisterAndSignUp();
 
+moviesLoading();
 initializeLibrary('watched');
