227a690d111dc16a5e79462f216c4f0f9a4c6607
Karwan87 <damianuszynski@gmail.com>
2023-05-27 17:48:21 +0200

    Update api.js
    tryind to change visible items per page

--- src/js/services/api.js
+++ src/js/services/api.js
@@ -1,23 +1,33 @@
 import axios from 'axios';
 import { API_KEY } from '../variables/constants';
+const itemsPerPage = 24; // results per page
 
 export const fetchTrendingMovies = async pageNumber => {
   const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
     params: {
       api_key: API_KEY,
       page: pageNumber,
+      itemsPerPage: itemsPerPage,
     },
   });
   return response;
 };
 
-export const fetchMoviesWithQuery = async searchQuery => {
+export const fetchMoviesWithQuery = async (searchQuery, page) => {
+  console.log('Search Query:', searchQuery);
+  console.log('Page:', page);
+
+  const start = (page - 1) * itemsPerPage;
   const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
     params: {
       api_key: API_KEY,
       query: searchQuery,
+      page: page,
+      itemsPerPage: itemsPerPage,
+      start: start,
     },
   });
+  console.log('API Response:', response);
   return response;
 };
 
